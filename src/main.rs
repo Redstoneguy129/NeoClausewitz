92deb44dd1ce5ee6c84b08493926baa43e33339f
Cameron Whyte <public@cameronwhyte.me>
2024-10-05 14:48:33 +0100

    Add debug_utils_loader and debug_call_back to NeoClausewitz constructor.

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ struct NeoClausewitz {
     _entry: ash::Entry,
     instance: ash::Instance,
     surface: ash::khr::surface::Instance,
+    debug_utils_loader: ash::ext::debug_utils::Instance,
+    debug_call_back: vk::DebugUtilsMessengerEXT,
 }
 
 impl NeoClausewitz {
@@ -17,11 +19,29 @@ impl NeoClausewitz {
         let entry = ash::Entry::linked();
         let instance = NeoClausewitz::create_instance(&entry, event_loop);
         let surface = ash::khr::surface::Instance::new(&entry, &instance);
+        let debug_info = vk::DebugUtilsMessengerCreateInfoEXT::default()
+            .message_severity(
+                vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
+            )
+            .message_type(
+                vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                    | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                    | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+            )
+            .pfn_user_callback(Some(vulkan_debug_callback));
+        let debug_utils_loader = ash::ext::debug_utils::Instance::new(&entry, &instance);
+        let debug_call_back = debug_utils_loader
+            .create_debug_utils_messenger(&debug_info, None)
+            .unwrap();
 
         NeoClausewitz {
             _entry: entry,
             instance,
             surface,
+            debug_utils_loader,
+            debug_call_back,
         }
     }
 
@@ -34,11 +54,35 @@ impl NeoClausewitz {
     }
 
     unsafe fn create_instance(entry: &ash::Entry, event_loop: &winit::event_loop::EventLoop<()>) -> ash::Instance {
-        let surface_extensions = ash_window::enumerate_required_extensions(event_loop.display_handle().unwrap().as_raw()).unwrap();
+        let layer_names = [std::ffi::CStr::from_bytes_with_nul_unchecked(
+                b"VK_LAYER_KHRONOS_validation\0",
+            )];
+        let layers_names_raw: Vec<*const std::ffi::c_char> = layer_names
+                .iter()
+                .map(|raw_name| raw_name.as_ptr())
+                .collect();
+
+        let mut extension_names =
+                ash_window::enumerate_required_extensions(event_loop.display_handle().unwrap().as_raw())
+                    .unwrap()
+                    .to_vec();
+            extension_names.push(ash::ext::debug_utils::NAME.as_ptr());
+
         let app_desc = vk::ApplicationInfo::default().api_version(vk::make_api_version(0, 1, 0, 0));
+
+        let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+                vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+            } else {
+                vk::InstanceCreateFlags::default()
+            };
+
         let instance_desc = vk::InstanceCreateInfo::default()
             .application_info(&app_desc)
-            .enabled_extension_names(surface_extensions);
+            .enabled_layer_names(&layers_names_raw)
+            .enabled_extension_names(&extension_names)
+            .flags(create_flags);
+
+
         entry.create_instance(&instance_desc, None).expect("Failed to create instance.")
     }
 
@@ -99,4 +143,32 @@ fn main() {
         let neo_clausewitz = NeoClausewitz::new(&event_loop);
         neo_clausewitz.main_loop(event_loop, window);
     }
+}
+
+unsafe extern "system" fn vulkan_debug_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'_>,
+    _user_data: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    let callback_data = *p_callback_data;
+    let message_id_number = callback_data.message_id_number;
+
+    let message_id_name = if callback_data.p_message_id_name.is_null() {
+        std::borrow::Cow::from("")
+    } else {
+        std::ffi::CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy()
+    };
+
+    let message = if callback_data.p_message.is_null() {
+        std::borrow::Cow::from("")
+    } else {
+        std::ffi::CStr::from_ptr(callback_data.p_message).to_string_lossy()
+    };
+
+    println!(
+        "{message_severity:?}:\n{message_type:?} [{message_id_name} ({message_id_number})] : {message}\n",
+    );
+
+    vk::FALSE
 }
